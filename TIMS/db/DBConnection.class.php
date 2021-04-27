c63740afc01b88f53452bacb64db8fd2b51d106a
saravanan <saravananm@somnoware.com>
2021-04-27 12:27:18 +0530

    TIMS-Intial Setpu

---
+++ TIMS/db/DBConnection.class.php
@@ -0,0 +1,61 @@
+<?php
+class DBConnection{
+	
+    public static $servername = "localhost";
+	public static $username = "root";
+	public static $password = "root";
+	public static $dbase = "KeltronDB";
+
+	static function  getConnect(){
+
+		$connection = new mysqli(self::$servername, self::$username, self::$password,self::$dbase );
+		echo "Connection ok";
+		return $connection;
+	}
+
+	static function execNonQery($sql){
+		try{
+			$result = -1;
+			$conn = self::getConnect();
+			if ($conn->connect_error) {
+	  				$result =  -2;
+				}
+			else{
+
+					if($conn->query($sql) == TRUE ){
+						$result = $conn->affected_rows ;
+						$conn->close();
+					}
+
+				}	
+		}
+		catch(Exception $e) {
+			// implement the exception log details
+  			$result = -3;
+		}
+	
+		return $result;
+	}
+
+	static function execQery($sql){
+		try{
+			$result = null;
+			$conn = self::getConnect();
+			if ($conn->connect_error) {
+	  				$result = null;
+				}
+			else{
+					$result = $conn->query($sql);
+					$conn->close();
+				}	
+		}
+		catch(Exception $e) {
+  			$result = null;
+		}
+	
+		return $result;
+	}
+	
+}
+
+?>
---
+++ TIMS/views/include/menu.php
@@ -0,0 +1,6 @@
+<ul style="list-style:none; padding:0px; line-height:24px;">
+                    <li><a href="student_view_all.php" style="color:#333;">Home</a></li>
+                    <li><a href="#" style="color:#333;">About</a></li>
+                    <li><a href="student_create.php" style="color:#333;">Student New</a></li>
+                    <li><a href="#" style="color:#333;">Course</a></li>
+                </ul>
